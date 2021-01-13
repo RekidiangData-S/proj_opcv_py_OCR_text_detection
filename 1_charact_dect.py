17a962270387d3537357981b239b712b9777ce0d
RekidiangData-S <69548352+RekidiangData-S@users.noreply.github.com>
2021-01-13 14:26:19 +0800

    Add files via upload

---
+++ 1_charact_dect.py
@@ -0,0 +1,14 @@
+import cv2
+import pytesseract
+import numpy as np
+from PIL import ImageGrab
+import time
+
+
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+img = cv2.imread('1.jpg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+print(pytesseract.image_to_string(img))
+
+cv2.imshow('img', img)
+cv2.waitKey(0)
